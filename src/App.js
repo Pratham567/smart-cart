7099c289ee809acc2040ecb350056c2ccf72cb93
Pratham Gupta <prathagu@cisco.com>
2024-09-07 16:07:36 +0530

    Add routes and link to the navbar items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import Navbar from "./Components/Navbar/Navbar";
+import Shop from "./Components/Pages/Shop";
+import Cart from "./Components/Pages/Cart";
+import LoginSignup from "./Components/Pages/LoginSignup";
+import ShopCategory from "./Components/Pages/ShopCategory";
+import Product from "./Components/Pages/Product";
+import Contact from "./Components/Pages/Contact";
+
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/men" element={<ShopCategory category="Men" />} />
+          <Route path="/women" element={<ShopCategory category="Women" />} />
+          <Route path="/kids" element={<ShopCategory category="Kids" />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+
+      </Router>
       <div className="content" >
         <h3>Welcome to the smartcart</h3>
       </div>
