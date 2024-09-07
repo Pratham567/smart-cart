cff066c842aa2e727344f3ec31c82361e0cee894
Pratham Gupta <prathagu@cisco.com>
2024-09-07 18:45:14 +0530

    Add component for offers

--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -1,11 +1,13 @@
 import Hero from "../Hero/Hero";
 import Trending from "../Trending/Trending";
+import Offers from "../Offers/Offers";
 
 const Shop = () => {
     return ( 
         <div>
             <Hero/>
             <Trending />
+            <Offers />
         </div>
      );
 }
