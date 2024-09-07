9135ba82536852f756565ef433934daac0b91d86
Pratham Gupta <prathagu@cisco.com>
2024-09-07 17:52:55 +0530

    Add component for trending in women

--- src/Components/Pages/Shop.jsx
+++ src/Components/Pages/Shop.jsx
@@ -1,9 +1,11 @@
 import Hero from "../Hero/Hero";
+import Trending from "../Trending/Trending";
 
 const Shop = () => {
     return ( 
         <div>
             <Hero/>
+            <Trending />
         </div>
      );
 }
