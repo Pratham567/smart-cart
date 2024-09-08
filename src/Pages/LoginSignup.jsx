8ac72dab76ea082c40275d34779fb32b0c166a0d
Pratham Gupta <prathagu@cisco.com>
2024-09-08 11:44:28 +0530

    Complete login signup UI

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,26 @@
+import './CSS/LoginSignup.css';
+
 const LoginSignup = () => {
-    return ( 
-        <div>
-            <h1>LoginSignup Page</h1>
+    return (
+        <div className="loginsignup">
+            <div className="loginsignup-container">
+                <h1>Sign Up</h1>
+                <form className="loginsignup-form">
+                    <input type="text" placeholder="Username" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <div className="terms">
+                        <input type="checkbox" id="terms" />
+                        <label htmlFor="terms">I accept the <a href="/terms">terms and conditions</a></label>
+                    </div>
+                    <button type="submit">Sign Up</button>
+                </form>
+                <p>
+                    Already have an account? <a href="/login">Login</a>
+                </p>
+            </div>
         </div>
-     );
-}
- 
+    );
+};
+
 export default LoginSignup;
