409eface9b5406e926a426a68c3081e1e49adf7e
Pratham Gupta <prathagu@cisco.com>
2024-09-08 14:35:45 +0530

    Add the description box in the Product Page

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,31 @@
+import './DescriptionBox.css';
+
+const DescriptionBox = () => {
+    return (
+        <div className="descriptionbox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">
+                    Description
+                </div>
+                <div className="descriptionbox-nav-box fade">
+                    Review (555)
+                </div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec odio vitae nunc
+                    scelerisque fringilla. Nullam nec purus nec nunc scelerisque fringilla. Nullam nec purus
+                    nec nunc scelerisque fringilla. Nullam nec purus nec nunc scelerisque fringilla. Nullam nec
+                    purus nec nunc scelerisque fringilla. Nullam nec purus nec nunc scelerisque fringilla. Nullam
+                    nec purus nec nunc scelerisque fringilla. Nullam nec purus nec nunc scelerisque fringilla.
+                    Nullam nec purus nec nunc scelerisque fringilla. Nullam nec purus nec nunc scelerisque fringilla.
+                    Nullam nec purus nec nunc scelerisque fringilla. Nullam nec purus nec nunc scelerisque fringilla.
+                    Nullam nec purus nec nunc scelerisque fringilla. Nullam nec purus nec nunc scelerisque fringilla.
+                    Nullam nec purus nec nunc scelerisque fringilla. Nullam nec purus nec nunc scelerisque fringilla.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default DescriptionBox;
